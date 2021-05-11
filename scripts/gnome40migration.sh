5f4470da2c8a5bd674b44b4613ff1b5ff17ce341
Chrysostomus <matti@manjaro.org>
2021-05-11 22:01:42 +0300

    Setup gnome40 update

---
+++ scripts/gnome40migration.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+dconf load / < /usr/share/gnome-shell/10_gnome40transition
+if ! [[ -e ~/.config/autostart/gnome40-transition.desktop ]] ; then
+  cp /etc/xdg/autostart/gnome40-transition.desktop ~/.config/autostart/gnome40-transition.desktop
+  echo "Hidden=true" >> ~/.config/autostart/gnome40-transition.desktop
+fi 
