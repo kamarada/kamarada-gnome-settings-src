c0317eeb78b6fd2c0bb6295f01ff2e1e3d8a486f
Mark Wagie <mark@manjaro.org>
2022-02-28 11:15:39 -0700

    scripts: drop gnome40migration.sh

--- scripts/gnome40migration.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if ! [[ -e /etc/.gnome40new ]]; then
-	dconf load / < /usr/share/gnome-shell/10_gnome40transition
-	if ! [[ -e ~/.config/autostart/gnome40-transition.desktop ]] ; then
-	  cp /etc/xdg/autostart/gnome40-transition.desktop ~/.config/autostart/gnome40-transition.desktop
-	  echo "Hidden=true" >> ~/.config/autostart/gnome40-transition.desktop
-	fi 
-fi
