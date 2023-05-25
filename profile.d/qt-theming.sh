c89db9e2143ca9c99fe4a8ac1de8ca802f624f2a
Mark Wagie <mark@manjaro.org>
2023-05-25 11:04:43 -0600

    profile.d: set Qt theming

---
+++ profile.d/qt-theming.sh
@@ -0,0 +1,13 @@
+# To use QGnomePlaform with AdwaitaQt style, comment out the following keys:
+
+# Available options: gtk2, gnome, kde, qt5ct, xcb
+
+# Note: Don't use in combination with QT_STYLE_OVERRIDE
+export QT_QPA_PLATFORMTHEME="qt5ct"
+
+# Available styles: HighContrastInverse, HighContrast, Adwaita-HighContrastInverse,
+# Adwaita-HighContrast, Adwaita-Dark, Adwaita, kvantum-dark, kvantum, qt5ct-style,
+# Windows, Fusion
+
+# Note: Don't use in combination with QT_QPA_PLATFORMTHEME
+#export QT_STYLE_OVERRIDE=""
